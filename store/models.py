203fbe711e1496019d9ef073613b1f4777e2c368
Lovekesh <20bec022@nith.ac.in>
2021-07-22 00:10:39 +0530

    All the templates modified,the settings,main urls and models of the store being modified

--- store/models.py
+++ store/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.expressions import F 
 # Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(User,null=True,blank=True,on_delete=models.CASCADE)
+    name = models.CharField(max_length=200,null=True)
+    email= models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False,null=True,blank=True)
+    
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer,on_delete=models.SET_NULL,null=True,blank=True)
+    date_ordered = models.DateField(auto_now_add=True)
+    digital = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=200,null=True)
