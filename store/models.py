796d1e3235cf7ce7552582b81ad3081a700a3003
Lovekesh <20bec022@nith.ac.in>
2021-08-02 19:26:41 +0530

    All the functionalities added

--- store/models.py
+++ store/models.py
@@ -16,7 +16,7 @@ class Customer(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
-    price = models.FloatField()
+    price = models.DecimalField()
     digital = models.BooleanField(default=False, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
@@ -42,17 +42,25 @@ class Order(models.Model):
         return str(self.id)
 
 	@property
-	def get_cart_total(self):
-                orderitems = self.orderitem_set.all()
-                total = sum([item.get_total for item in orderitems])
-                return total
+	def shipping(self):
+	        shipping = False
+		orderitems = self.orderitem_set.all()
+		for i in orderitems:
+			        if i.product.digital == False:
+				        shipping = True
+		return shipping
 
     @property
     def get_cart_items(self):
             orderitems = self.orderitem_set.all()
             total = sum([item.quantity for item in orderitems])
             return total             
-
+    
+    @property
+    def get_cart_total(self):
+           orderitems = self.orderitem_set.all()
+           total = sum([item.get_total for item in orderitems])
+           return total
 
 
 class OrderItem(models.Model):
